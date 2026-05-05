48800b868fd50f67f25b9eaf474594ead2b02adb
alan5411 <alan.livinsky@gmail.com>
2026-05-05 12:22:39 -0300

    refactor(audit): correct supervisor auditor group name identifier
    Fix typo in group name from 'z_supervisosr_auditor_recetas' to 'z_supervisor_auditoria_recetas' and update all references in Python and XML files.

--- z_001_prescription_audit.py
+++ z_001_prescription_audit.py
@@ -155,7 +155,7 @@ class MedicationAudit(ModelSQL, ModelView):
         ModelData = pool.get('ir.model.data')
         try:
             group_id = ModelData.get_id(
-                'z_001_prescription_audit', 'z_supervisosr_auditor_recetas')
+                'z_001_prescription_audit', 'z_supervisor_auditoria_recetas')
         except KeyError:
             return False
         current_user = User(Transaction().user)
